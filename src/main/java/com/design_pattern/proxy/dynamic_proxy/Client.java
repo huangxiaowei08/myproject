aa2c6c77384a1d2830785d1e0a827967e320e097
unknown <xiaowei.huang@newtouch.cn>
2018-04-02 15:06:58 +0800

    完善注释

--- src/main/java/com/design_pattern/proxy/dynamic_proxy/Client.java
+++ src/main/java/com/design_pattern/proxy/dynamic_proxy/Client.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Proxy;
 /**
  * https://blog.csdn.net/Rongbo_J/article/details/49203289  JDK和Cglib实现动态代理实例及优缺点分析
  * https://blog.csdn.net/qq_27093465/article/details/53340513 Java动态代理模式jdk和cglib的2种实现以及二者的区别(AOP面向切面的前奏)
+ * spring框架用的是cglib实现的动态代理
  */
 
 public class Client {
